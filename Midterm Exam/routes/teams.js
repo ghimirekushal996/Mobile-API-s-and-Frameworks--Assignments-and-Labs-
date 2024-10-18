8fe7703b88d038741996a99f9968cdfbec7ae16f
ghimirekushal996 <ghimirekushal1996@gmail.com>
2024-10-18 18:43:56 -0400

    created routes

---
+++ Midterm Exam/routes/teams.js
@@ -0,0 +1,15 @@
+/**
+ * File name: teams.js
+ * Student Name: Kushal Ghimire
+ * StudentID: 200555588
+ * Date: Octopber 18, 2024
+ */
+
+const express = require('express');
+const router = express.Router();
+
+
+// Define the endpoint for fetching all teams
+router.get('/', findAllTeams);
+
+module.exports = router;
