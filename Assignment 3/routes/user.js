b242ace0f4a581838299a8d3424b631a47abba3c
ghimirekushal996 <ghimirekushal1996@gmail.com>
2024-11-15 15:59:34 -0500

    created register routes and hased the password for security

---
+++ Assignment 3/routes/user.js
@@ -0,0 +1,30 @@
+// routes/user.js
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const router = express.Router();
+
+// Register API
+router.post('/register', async (req, res) => {
+    const { username, email, password } = req.body;
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const newUser = new User({
+        username,
+        email,
+        password: hashedPassword,
+    });
+
+    try {
+        await newUser.save();
+        res.status(201).json({ message: 'User registered successfully.' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error registering user.', error });
+    }
+});
+
+module.exports = router;
