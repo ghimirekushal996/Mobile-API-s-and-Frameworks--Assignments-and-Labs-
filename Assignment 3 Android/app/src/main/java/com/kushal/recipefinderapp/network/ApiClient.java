53d25b51f2fcb7fc16e0dad69009369b48198c3e
ghimirekushal996 <ghimirekushal1996@gmail.com>
2024-11-15 18:57:11 -0500

    code refactor

--- Assignment 3 Android/app/src/main/java/com/kushal/recipefinderapp/network/ApiClient.java
+++ Assignment 3 Android/app/src/main/java/com/kushal/recipefinderapp/network/ApiClient.java
@@ -6,7 +6,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String BASE_URL = "http://your-api-url-here/";
+    private static final String BASE_URL = "http://localhost:3000/api/routes/recipes";
     private static Retrofit retrofit;
 
     public static Retrofit getClient() {
