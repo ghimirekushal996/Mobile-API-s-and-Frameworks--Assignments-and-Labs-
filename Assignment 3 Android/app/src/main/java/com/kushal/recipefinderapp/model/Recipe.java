04a2ce8661efe9192cc7f70a11a8d41d260b1b90
ghimirekushal996 <ghimirekushal1996@gmail.com>
2024-11-15 20:44:56 -0500

    model setup

--- Assignment 3 Android/app/src/main/java/com/kushal/recipefinderapp/model/Recipe.java
+++ Assignment 3 Android/app/src/main/java/com/kushal/recipefinderapp/model/Recipe.java
@@ -1,34 +1,89 @@
 package com.kushal.recipefinderapp.model;
 
 
-import com.google.gson.annotations.SerializedName;
+import java.util.List;
 
 public class Recipe {
-    @SerializedName("recipeName")
+    private String _id;
     private String recipeName;
-
-    @SerializedName("cuisine")
+    private List<String> ingredients;
+    private int cookingTime;
+    private String difficulty;
     private String cuisine;
-
-    @SerializedName("description")
     private String description;
-
-    @SerializedName("photoLink")
     private String photoLink;
+    private double averageRating;
+
+    // Getters and setters
+    public String getId() {
+        return _id;
+    }
+
+    public void setId(String _id) {
+        this._id = _id;
+    }
 
     public String getRecipeName() {
         return recipeName;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public int getCookingTime() {
+        return cookingTime;
+    }
+
+    public void setCookingTime(int cookingTime) {
+        this.cookingTime = cookingTime;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
     }
 
     public String getCuisine() {
         return cuisine;
     }
 
+    public void setCuisine(String cuisine) {
+        this.cuisine = cuisine;
+    }
+
     public String getDescription() {
         return description;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public String getPhotoLink() {
         return photoLink;
+    }
+
+    public void setPhotoLink(String photoLink) {
+        this.photoLink = photoLink;
+    }
+
+    public double getAverageRating() {
+        return averageRating;
+    }
+
+    public void setAverageRating(double averageRating) {
+        this.averageRating = averageRating;
     }
 }
