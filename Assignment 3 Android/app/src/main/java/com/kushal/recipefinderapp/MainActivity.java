2d199cb85d8336b4b8dca8d33f4e0e70d3cba1db
ghimirekushal996 <ghimirekushal1996@gmail.com>
2024-11-15 20:46:35 -0500

    setting up main activity

--- Assignment 3 Android/app/src/main/java/com/kushal/recipefinderapp/MainActivity.java
+++ Assignment 3 Android/app/src/main/java/com/kushal/recipefinderapp/MainActivity.java
@@ -1,24 +1,65 @@
 package com.kushal.recipefinderapp;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.kushal.recipefinderapp.adapter.RecipeAdapter;
+import com.kushal.recipefinderapp.api.ApiClient;
+import com.kushal.recipefinderapp.api.ApiService;
+import com.kushal.recipefinderapp.model.Recipe;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecipeAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // get all recipe from below function
+        fetchRecipes();
+    }
+
+    private void fetchRecipes() {
+        ApiService apiService = ApiClient.getClient().create(ApiService.class);
+        Call<List<Recipe>> call = apiService.getAllRecipes();
+
+        call.enqueue(new Callback<List<Recipe>>() {
+            @Override
+            public void onResponse(Call<List<Recipe>> call, Response<List<Recipe>> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    adapter = new RecipeAdapter(response.body());
+                    recyclerView.setAdapter(adapter);
+                } else {
+                    Toast.makeText(MainActivity.this, "Failed to load recipes", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Recipe>> call, Throwable t) {
+                System.out.println("Issue:"+t.getMessage());
+                Toast.makeText(MainActivity.this, "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
