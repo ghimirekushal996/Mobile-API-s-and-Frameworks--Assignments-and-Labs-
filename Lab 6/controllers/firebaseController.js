61703b634726541c2243fe9d99d327c4a3b59633
ghimirekushal996 <ghimirekushal1996@gmail.com>
2024-11-29 15:09:04 -0500

    Created firebaseController.js middleware to verify Firebase ID token

---
+++ Lab 6/controllers/firebaseController.js
@@ -0,0 +1,23 @@
+// firebaseController.js
+
+const admin = require("./firebaseModel");
+
+const verifyToken = async (req, res, next) => {
+  const authHeader = req.header("Authorization");
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Token is required" });
+  }
+
+  const token = authHeader.split("Bearer ")[1];
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    req.user = decodedToken;
+    next(); // Token is valid, move to the next route handler
+  } catch (error) {
+    res.status(401).json({ error: "Invalid or expired token" });
+  }
+};
+
+module.exports = verifyToken;
