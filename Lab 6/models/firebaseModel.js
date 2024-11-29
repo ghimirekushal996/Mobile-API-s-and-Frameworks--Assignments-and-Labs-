71f8ddbb6f40bce17a34fc9343f0b9b3fc409035
ghimirekushal996 <ghimirekushal1996@gmail.com>
2024-11-29 15:03:02 -0500

    Set up project structure and installed dependencies as well as created firebaseModel.js to initialize Firebase Admin SDK

---
+++ Lab 6/models/firebaseModel.js
@@ -0,0 +1,12 @@
+// firebaseModel.js
+
+const admin = require("firebase-admin");
+const serviceAccount = require("./firebase-adminsdk.json"); // Firebase service account key
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://mdev1004-lab6-default-rtdb.firebaseio.com",
+  });
+}
+module.exports = admin;
