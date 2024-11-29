28879aab155855d6402a9df7f4d52e24cbcee07e
ghimirekushal996 <ghimirekushal1996@gmail.com>
2024-11-29 16:17:17 -0500

    firebase admin sdk json has been added

--- Lab 6/routes/firebaseRoutes.js
+++ Lab 6/routes/firebaseRoutes.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const router = express.Router();
-const verifyToken = require("./controllers/firebaseController");
+const verifyToken = require("../controllers/firebaseController");
 
 // Protected route (only accessible with valid token)
 router.get("/protected", verifyToken, (req, res) => {
