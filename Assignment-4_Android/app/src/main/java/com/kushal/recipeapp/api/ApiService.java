4038ca1bb424380a27d83776fc39d160e226b3a2
ghimirekushal996 <ghimirekushal1996@gmail.com>
2024-12-06 13:24:53 -0500

    register route defined in ApiService Interface

--- Assignment-4_Android/app/src/main/java/com/kushal/recipeapp/api/ApiService.java
+++ Assignment-4_Android/app/src/main/java/com/kushal/recipeapp/api/ApiService.java
@@ -1,4 +1,16 @@
 package com.kushal.recipeapp.api;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
 public interface ApiService {
-}
+    @POST("auth/register")
+    Call<Void> registerUser(@Body User user);
