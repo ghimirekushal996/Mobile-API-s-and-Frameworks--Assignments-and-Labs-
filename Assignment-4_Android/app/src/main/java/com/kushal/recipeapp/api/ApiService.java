c28e837bc79bdd0fcd74d9cb9d0d666073a57407
ghimirekushal996 <ghimirekushal1996@gmail.com>
2024-12-06 14:44:44 -0500

    Updating recipe route has been made

--- Assignment-4_Android/app/src/main/java/com/kushal/recipeapp/api/ApiService.java
+++ Assignment-4_Android/app/src/main/java/com/kushal/recipeapp/api/ApiService.java
@@ -1,7 +1,9 @@
 package com.kushal.recipeapp.api;
 
+import com.kushal.recipeapp.models.AddRecipe;
 import com.kushal.recipeapp.models.LoginRequest;
 import com.kushal.recipeapp.models.LoginResponse;
+import com.kushal.recipeapp.models.Recipe;
 import com.kushal.recipeapp.models.User;
 
 import java.util.List;
@@ -27,4 +29,10 @@ public interface ApiService {
 
     @POST("recipe/create")
     Call<AddRecipe> createRecipe(@Header("Authorization") String token, @Body AddRecipe recipe);
-}
+
+    @PUT("recipe/{id}")  // Define the PUT request for updating a recipe
+    Call<AddRecipe> updateRecipe(
+            @Header("Authorization") String authorizationToken,
+            @Path("id") String recipeId,  // Path parameter for recipe ID
+            @Body AddRecipe updatedRecipe  // Request body with updated recipe data
+    );
