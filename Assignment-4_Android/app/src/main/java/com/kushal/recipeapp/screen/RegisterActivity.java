92a6285adf9e99b65788b58bee8a5647ef5d8a5d
ghimirekushal996 <ghimirekushal1996@gmail.com>
2024-12-06 13:41:49 -0500

    Register Activity updated

--- Assignment-4_Android/app/src/main/java/com/kushal/recipeapp/screen/RegisterActivity.java
+++ Assignment-4_Android/app/src/main/java/com/kushal/recipeapp/screen/RegisterActivity.java
@@ -30,4 +30,46 @@ public class RegisterActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+        
+        etFullName = findViewById(R.id.etFullName);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        registerController = new RegisterController(getApplicationContext());
+
+        btnRegister.setOnClickListener(view -> {
+            String fullname = etFullName.getText().toString().trim();
+            String email = etEmail.getText().toString().trim();
+            String password = etPassword.getText().toString().trim();
+
+            if (fullname.isEmpty() || email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "All fields are required", Toast.LENGTH_SHORT).show();
+            } else {
+                User user = new User(fullname, email, password);
+                registerController.registerUser(user, new RegisterController.RegisterCallback() {
+                    @Override
+                    public void onSuccess(String message) {
+                        Toast.makeText(RegisterActivity.this, message, Toast.LENGTH_SHORT).show();
+                        // Handle successful registration, e.g., navigate to the Login screen
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Toast.makeText(RegisterActivity.this, error, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onUserExists(String message) { // Handle "User already exists"
+                        Toast.makeText(RegisterActivity.this, message, Toast.LENGTH_SHORT).show();
+                        // Navigate back to the Login screen
+                        startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                        finish();
+                    }
+                });
+            }
+        });
+    }
+
+}
 
