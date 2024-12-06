9542f6e7b6e549a7c1ca71c70ebf656f6bb4dde5
ghimirekushal996 <ghimirekushal1996@gmail.com>
2024-12-06 14:11:11 -0500

    Functions called in Splash Activity and updated

--- Assignment-4_Android/app/src/main/java/com/kushal/recipeapp/screen/SplashActivity.java
+++ Assignment-4_Android/app/src/main/java/com/kushal/recipeapp/screen/SplashActivity.java
@@ -1,4 +1,38 @@
 package com.kushal.recipeapp.screen;
 
-public class SplashActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.kushal.recipeapp.sharedpreference.SharedPreferenceManager;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final int SPLASH_DELAY = 2000; // 2 seconds delay
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        // Initialize SharedPreferenceManager
+        SharedPreferenceManager sharedPreferenceManager = new SharedPreferenceManager(this);
+
+        // Delay for SPLASH_DELAY and then check token
+        new Handler().postDelayed(() -> {
+//            String token = sharedPreferenceManager.getToken();
+            Intent intent;
+//            if (token != null && !token.isEmpty()) {
+//                // Token exists, go to MainActivity
+//                intent = new Intent(SplashActivity.this, MainActivity.class);
+//            } else {
+//                // Token does not exist, go to LandingActivity
+//                intent = new Intent(SplashActivity.this, LandingActivity.class);
+//            }
+            intent = new Intent(SplashActivity.this, LandingActivity.class);
+            startActivity(intent);
+            finish(); // Close SplashActivity
+        }, SPLASH_DELAY);
+    }
 }
