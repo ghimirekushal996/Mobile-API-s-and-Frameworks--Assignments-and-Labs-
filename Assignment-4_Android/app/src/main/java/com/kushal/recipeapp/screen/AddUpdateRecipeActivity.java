a9ae05b3b227cbf96050b11ad0f4c4b9ea38d9d3
ghimirekushal996 <ghimirekushal1996@gmail.com>
2024-12-06 14:50:17 -0500

    AddUpdateRecipeActivity created

---
+++ Assignment-4_Android/app/src/main/java/com/kushal/recipeapp/screen/AddUpdateRecipeActivity.java
@@ -0,0 +1,26 @@
+package com.kushal.recipeapp.screen;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.kushal.recipeapp.R;
+
+public class AddUpdateRecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_add_update_recipe);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
