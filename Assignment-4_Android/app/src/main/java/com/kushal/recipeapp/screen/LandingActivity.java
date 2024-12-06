e0cf6152a6b12849e28e9331409f3db2a912fa20
ghimirekushal996 <ghimirekushal1996@gmail.com>
2024-12-06 14:13:32 -0500

    Update on LandingActivity

--- Assignment-4_Android/app/src/main/java/com/kushal/recipeapp/screen/LandingActivity.java
+++ Assignment-4_Android/app/src/main/java/com/kushal/recipeapp/screen/LandingActivity.java
@@ -1,4 +1,40 @@
 package com.kushal.recipeapp.screen;
 
-public class LandingActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LandingActivity extends AppCompatActivity {
+
+    private Button btnLogin, btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+        // Initialize Views
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        // Login Button Click Listener
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(LandingActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
+
+        // Register Button Click Listener
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(LandingActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+    }
 }
