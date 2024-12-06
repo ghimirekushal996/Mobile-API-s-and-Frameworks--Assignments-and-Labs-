46fef5149b089c0ef7af96a5ab7b89bacd2915a0
ghimirekushal996 <ghimirekushal1996@gmail.com>
2024-12-06 13:36:06 -0500

    User file updated

--- Assignment-4_Android/app/src/main/java/com/kushal/recipeapp/models/User.java
+++ Assignment-4_Android/app/src/main/java/com/kushal/recipeapp/models/User.java
@@ -1,4 +1,26 @@
 package com.kushal.recipeapp.models;
 
 public class User {
+    private String fullname;
+    private String email;
+    private String password;
+
+    public User(String fullname, String email, String password) {
+        this.fullname = fullname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
+
