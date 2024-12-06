8c961698af5c599914f8fd62264bba5afb356292
ghimirekushal996 <ghimirekushal1996@gmail.com>
2024-12-06 13:57:28 -0500

    LoginResponse updated

---
+++ Assignment-4_Android/app/src/main/java/com/kushal/recipeapp/models/LoginResponse.java
@@ -0,0 +1,15 @@
+package com.kushal.recipeapp.models;
+
+public class LoginResponse {
+    private String message;
+    private String token;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
+}
