fa69eb6f4f2330da71b3e5da56456d18302ad9c6
ghimirekushal996 <ghimirekushal1996@gmail.com>
2024-12-06 02:45:49 -0500

    Created models.js inside the models folder

---
+++ Assignment 4(Secure API with CRUD)/models/models.js
@@ -0,0 +1,57 @@
+/**
+ * File name: models.js
+ * Student Name: Kushal Ghimire
+ * StudentID: 200555588
+ * Date: December 06, 2024
+ */
+
+
+
+// Import Mongoose
+const mongoose = require('mongoose');
+
+const recipeSchema = new mongoose.Schema({
+    recipeName: {
+        type: String,
+        required: true,
+    },
+    ingredients: {
+        type: [String], // Array of strings for ingredients
+        required: true,
+    },
+    cookingTime: {
+        type: Number,
+        required: true,
+    },
+    difficulty: {
+        type: String,
+        enum: ['Easy', 'Medium', 'Hard'], // Valid options for difficulty
+        required: true,
+    },
+    cuisine: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    photoLink: {
+        type: String,
+        required: true,
+    },
+    averageRating: {
+        type: Number,
+        min: 0,
+        max: 5,
+        required: true,
+    },
+}, {
+    timestamps: true, // Adds createdAt and updatedAt timestamps
+});
+
+// Create the Recipe model
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+// Export the model
+module.exports = Recipe;
