57b43da2d934d32e1389654a6bae6733920a9589
ghimirekushal996 <ghimirekushal1996@gmail.com>
2024-12-06 02:51:28 -0500

    Created auth.js middleware to verify Token

---
+++ Assignment 4(Secure API with CRUD)/middleware/auth.js
@@ -0,0 +1,29 @@
+/**
+ * File name: auth.js
+ * Student Name: Kushal Ghimire
+ * StudentID: 200555588
+ * Date: December 06, 2024
+ */
+
+
+// middleware/auth.js
+const jwt = require('jsonwebtoken');
+
+// Middleware to verify JWT token
+const authenticateToken = (req, res, next) => {
+    const token = req.headers['authorization'];
+    const tokenWithoutBearer = token.startsWith('Bearer ') ? token.slice(7) : token;
+    if (!tokenWithoutBearer) {
+        return res.status(401).json({ message: 'Access denied. No token provided.' });
+    }
+
+    try {
+        const decoded = jwt.verify(tokenWithoutBearer, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        res.status(400).json({ message: 'Invalid token.' });
+    }
+};
+
+module.exports = authenticateToken;
