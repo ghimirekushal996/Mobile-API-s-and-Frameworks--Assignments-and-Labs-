60d83f1ece84e4d9abf92b7e9ab5a7d727091feb
ghimirekushal996 <ghimirekushal1996@gmail.com>
2024-12-06 13:40:57 -0500

    Function created and called in register activity

--- Assignment-4_Android/app/src/main/java/com/kushal/recipeapp/screen/RegisterActivity.java
+++ Assignment-4_Android/app/src/main/java/com/kushal/recipeapp/screen/RegisterActivity.java
@@ -1,4 +1,33 @@
 package com.kushal.recipeapp.screen;
 
-public class RegisterActivity {
-}
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class RegisterActivity extends AppCompatActivity {
+    private EditText etFullName, etEmail, etPassword;
+    private Button btnRegister;
+    private RegisterController registerController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
