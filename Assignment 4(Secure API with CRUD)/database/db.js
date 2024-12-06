c32ba778e8b9318b81809e5287b3568460e86011
ghimirekushal996 <ghimirekushal1996@gmail.com>
2024-12-06 02:42:33 -0500

    Created environment variable(.env file) and db.js file inside database folder

---
+++ Assignment 4(Secure API with CRUD)/database/db.js
@@ -0,0 +1,25 @@
+/**
+ * File name: db.js
+ * Student Name: Kushal Ghimire
+ * StudentID: 200555588
+ * Date: December 06, 2024
+ */
+
+
+const mongoose = require('mongoose');
+
+// Function to connect to MongoDB
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.error('Error connecting to MongoDB:', err);
+        process.exit(1); // Exit the process if connection fails
+    }
+};
+
+module.exports = connectDB;
