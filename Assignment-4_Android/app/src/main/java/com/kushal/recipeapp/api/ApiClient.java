d3ce81cf25ca175cfed6d88d9563ed4cc2372b11
ghimirekushal996 <ghimirekushal1996@gmail.com>
2024-12-06 13:19:45 -0500

    Updated Api Client

--- Assignment-4_Android/app/src/main/java/com/kushal/recipeapp/api/ApiClient.java
+++ Assignment-4_Android/app/src/main/java/com/kushal/recipeapp/api/ApiClient.java
@@ -1,2 +1,47 @@
 package com.kushal.recipeapp.api;
 
+import android.content.Context;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.io.IOException;
+
+public class ApiClient {
+    private static final String BASE_URL = "http://192.168.2.34:3001/"; // Your base URL
+    public static final String IMAGE_URL = "http://192.168.2.34:3001"; // Image URL
+    private static Retrofit retrofit;
+
+    public static Retrofit getClient(Context context) {
+        if (retrofit == null) {
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(new Interceptor() {
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            // Get token from SharedPreferenceManager
+                            SharedPreferenceManager sharedPreferenceManager = new SharedPreferenceManager(context);
+                            String token = sharedPreferenceManager.getToken();
+
+                            Request originalRequest = chain.request();
+                            Request.Builder requestBuilder = originalRequest.newBuilder()
+                                    .header("Authorization", token != null ? "Bearer " + token : "");
+
+                            Request request = requestBuilder.build();
+                            return chain.proceed(request);
+                        }
+                    })
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
