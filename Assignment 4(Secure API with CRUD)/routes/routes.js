1eb16c1d95ffed5a19e77d7b069f71cfcd48ddac
ghimirekushal996 <ghimirekushal1996@gmail.com>
2024-12-06 03:02:26 -0500

    feat: routes for creating recipe

---
+++ Assignment 4(Secure API with CRUD)/routes/routes.js
@@ -0,0 +1,40 @@
+/**
+ * File name: routes.js
+ * Student Name: Kushal Ghimire
+ * StudentID: 200555588
+ * Date: November 15, 2024
+ */
+
+const express = require('express');
+const Recipe = require('../models/models');
+const authenticateToken = require('../middleware/auth');
+
+const router = express.Router();
+
+// Create a new recipe
+router.post('/recipes', authenticateToken, async (req, res) => {
+    try {
+        const { recipeName, ingredients, cookingTime, difficulty, cuisine, description, photoLink, averageRating } = req.body;
+
+        const newRecipe = new Recipe({
+            recipeName,
+            ingredients,
+            cookingTime,
+            difficulty,
+            cuisine,
+            description,
+            photoLink,
+            averageRating,
+        });
+
+        await newRecipe.save();
+        res.status(201).json({ message: 'Recipe created successfully.', data: newRecipe });
+    } catch (error) {
+        res.status(500).json({ message: 'Error creating recipe.', error });
+    }
+});
+
+
+
+module.exports = router;
+
