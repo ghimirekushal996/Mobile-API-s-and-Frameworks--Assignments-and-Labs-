8f3547b30425dff6f67d86a407134e665c5dd84e
ghimirekushal996 <ghimirekushal1996@gmail.com>
2024-12-06 14:07:12 -0500

    Updated SharedPreferenceManager

--- Assignment-4_Android/app/src/main/java/com/kushal/recipeapp/sharedpreference/SharedPreferenceManager.java
+++ Assignment-4_Android/app/src/main/java/com/kushal/recipeapp/sharedpreference/SharedPreferenceManager.java
@@ -1,4 +1,32 @@
 package com.kushal.recipeapp.sharedpreference;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class SharedPreferenceManager {
+    private static final String PREF_NAME = "user_session";
+    private static final String KEY_TOKEN = "token";
+
+    private final SharedPreferences sharedPreferences;
+    private final SharedPreferences.Editor editor;
+
+    public SharedPreferenceManager(Context context) {
+        sharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+    }
+
+    public void saveToken(String token) {
+        editor.putString(KEY_TOKEN, token);
+        editor.apply();
+    }
+
+    public String getToken() {
+        return sharedPreferences.getString(KEY_TOKEN, null);
+    }
+
+    public void clearToken() {
+        editor.remove(KEY_TOKEN);
+        editor.apply();
+    }
 }
+
